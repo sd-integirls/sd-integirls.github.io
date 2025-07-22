286edeaf77ed2b618d9853c76b59fa38d81fc2c5
Kaursome10 <simrankaursome@gmail.com>
2025-07-21 22:45:59 -0700

    Add files via upload

---
+++ layout-loader.js
@@ -0,0 +1,17 @@
+function loadContent(page) {
+  fetch(page)
+    .then(response => response.text())
+    .then(data => document.getElementById('main-content').innerHTML = data);
+}
+
+// Load header and footer on page load
+fetch('header.html')
+  .then(response => response.text())
+  .then(data => document.getElementById('header').innerHTML = data);
+
+fetch('footer.html')
+  .then(response => response.text())
+  .then(data => document.getElementById('footer').innerHTML = data);
+
+// Load home page content by default
+loadContent('home.html');
